7e6c50085259dff5cbd51010d9fea498f6ebeadf
Christopher Cid <iamcid17@gmail.com>
2020-03-31 01:32:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
     artist = filename.split(" - ")[2]
-    
+    new_song = 
   end
   
   def artist_name=(name)
