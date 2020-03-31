b289cfc4dfaba6ee603a6a7031300b4c3127e5dc
Christopher Cid <iamcid17@gmail.com>
2020-03-31 00:49:15 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    @files
+  end
+  
+  def import(list_of_filenames)
+    
+  end
+end
